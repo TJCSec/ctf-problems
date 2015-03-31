477f4b3b266531d45c7c00ef39adea376c6715d7
Samuel Kim <ubuntor2000@gmail.com>
2015-03-31 05:44:19 -0400

    make rot13 solvable

--- rot13/src/rot13.c
+++ rot13/src/rot13.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include <string.h>
 
+// gcc's variable reordering messes with this problem
+char tmp[256] = {0};
+int end = 0;
+int size = 0;
+
 int rotinput(char* t, int size) {
     int i;
     int c;
     for (i = 0; i < size-1; i++) {
         c = getchar();
-        if ('A' <= c && c <= 'Z') {
-            c = (c-'A'+13)%26 + 'A';
-        } else if ('a' <= c && c <= 'z') {
-            c = (c-'a'+13)%26 + 'a';
-        }
+
         if (c == '\n' || c == EOF) {
             if (c == '\n') {
                 t[i] = '\n';
@@ -19,21 +20,25 @@ int rotinput(char* t, int size) {
             t[i] = 0;
             break;
         }
+
+        if ((c >= 'a' && c < 'a'+13) || (c >= 'A' && c < 'A'+13)) {
+            c += 26;
+        }
+
+        c -= 13;
         t[i] = c;
     }
+
     return i;
 }
 
 int main() {
     setbuf(stdout, NULL);
-
     char buf[256] = {0};
-    char tmp[256] = {0};
-    int end = 0;
-    int size = 0;
     printf("ROT13-ATOR\n");
     printf("Input strings. Send an empty newline to end.\n");
     while (strcmp(tmp,"\n")) {
+        memset(tmp, 0, 256);
         size = rotinput(tmp, 256);
         if (strlen(tmp) + strlen(buf) < 256) {
             memcpy(buf + end, tmp, size);
@@ -43,5 +48,5 @@ int main() {
             break;
         }
     }
-    printf("%s",buf);
+    printf("%s\n",buf);
 }
