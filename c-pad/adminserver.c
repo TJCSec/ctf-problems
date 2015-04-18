b08bf9b2af44d5715778eca06adb2b5146683d9d
cdeboor <2015cdeboor@tjhsst.edu>
2015-04-18 19:41:12 -0400

    move data to more descriptive header name

--- c-pad/adminserver.c
+++ c-pad/adminserver.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 
-#include "data.h"
+#include "msgstructs.h"
 
 char *lineptr = NULL;
 size_t linesize = 0;
--- c-pad/worker.c
+++ c-pad/worker.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include "data.h"
+#include "msgstructs.h"
 
 double ACCESS;
 
