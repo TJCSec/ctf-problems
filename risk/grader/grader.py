7ea1faa22eed2701fc1f432f6e2581fac38b864e
The Dank Meme Master <fwilson@fwilson.me>
2015-04-21 21:14:35 -0400

    format risk

---
+++ risk/grader/grader.py
@@ -0,0 +1,6 @@
+def grade(a, k):
+    if k == "rekt_by_bin":
+        return True, "Correct!"
+    elif k == "bfrewruotonbu_oodom_fluiswemeo_rollonhthk..gedyc":
+        return False, "Not quite, but you're close."
+    return False, "Nope."
