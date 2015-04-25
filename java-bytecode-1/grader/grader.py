374952143943a019261767132bc8ab2acf22ebeb
The Dank Meme Master <fwilson@fwilson.me>
2015-04-25 00:52:46 -0400

    fix grader for java bytecode 1

--- java-bytecode-1/grader/grader.py
+++ java-bytecode-1/grader/grader.py
@@ -1,5 +1,5 @@
 def grade(arg, flag):
-    if '0528691432349' in flag:
+    if '0528691432349' in flag or '025869143231899' in flag:
         return True, 'You either know how to bytecode or found a program that does'
     else:
         return False, 'Incorrect'
