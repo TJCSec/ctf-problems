48e0e59931d169a33db95b5397801cd9fc8b3430
Samuel Damashek <samuel.damashek@gmail.com>
2015-04-25 19:03:29 -0400

    Grader and description update

--- short_url/grader/grader.py
+++ short_url/grader/grader.py
@@ -1,4 +1,4 @@
 def grade(arg, key):
-    if "w0w_ur1_sh0rt3n!ng_1s_gr3at!!" in key:
+    if "w0w_y0u_d1d_s0m3_u@r#l_sh0rt3n1ng" in key:
         return True, "Correct"
     return False, "Incorrect"
